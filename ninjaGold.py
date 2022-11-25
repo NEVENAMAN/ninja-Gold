d2c7105637cd0c1fb7b5a3dd7c12e6c2d1584ffa
Neven Amen <nevenamen2000@gmail.com>
2022-11-25 13:58:53 +0200

    ninja-Gold2

--- ninjaGold.py
+++ ninjaGold.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 import random
 from flask import Flask,render_template,session,request,redirect
 app = Flask(__name__)
@@ -13,26 +13,38 @@ def index():
     session['casinoGold'] = 0
     session['activites'] = ''
     session['activitesCasino'] = ''
+    session['sortActivite']=[]
+    session['casinoCurrentGold']=[]
     return render_template("index.html")
 
 @app.route('/procces_money',methods=['post'])
 def procces_money():
+    activeCurrentGold = None
+    casinoCurrentGold = None
     if request.form['which_form'] =='farm':
         session['farmGold'] = random.randint(10,15)
         session['totalGold'] += int(session['farmGold'])
-        session['activites'] += 'Earned '+ str(int(session['farmGold'])) +' golds from the farm !  '+ str(datetime.datetime.now())+' <br>'
+        activeCurrentGold ='Earned '+ str(int(session['farmGold'])) +' golds from the farm !  '+ str(datetime.now())
+        
     elif request.form['which_form'] == 'cave':
         session['caveGold'] = random.randint(5,10)
         session['totalGold'] += int(session['caveGold'])
-        session['activites'] += 'Earned ' + str(int(session['caveGold'])) + ' golds from the cave !  ' + str(datetime.datetime.now())+' <br>'
+        activeCurrentGold ='Earned ' + str(int(session['caveGold'])) + ' golds from the cave !  ' + str(datetime.now())
+
     elif request.form['which_form'] == 'house':
         session['houseGold'] = random.randint(2,5)
         session['totalGold'] += int(session['houseGold'])
-        session['activites'] += 'Earned ' + str(int(session['houseGold'])) + ' golds from the house !  ' + str(datetime.datetime.now()) + ' <br>'
+        activeCurrentGold ='Earned ' + str(int(session['houseGold'])) + ' golds from the house !  ' + str(datetime.now()) 
+       
     elif request.form['which_form'] == 'casino':
         session['casinoGold'] = random.randint(-50,50)
         session['totalGold'] += int(session['casinoGold'])
-        session['activitesCasino'] += 'Earned ' + str(int(session['casinoGold'])) + ' golds from the casino !  ' + str(datetime.datetime.now())+' <br>'
+        casinoCurrentGold = 'Earned ' + str(int(session['casinoGold'])) + ' golds from the casino !  ' + str(datetime.now())
+    
+    if casinoCurrentGold!=None:     
+        session['casinoCurrentGold'].insert(0,casinoCurrentGold)
+    if activeCurrentGold!=None:
+        session['sortActivite'].insert(0,activeCurrentGold)
     return redirect('/goGold')
 
 @app.route('/goGold')
